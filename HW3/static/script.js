217d2cbe72eb0320ab64410800c6f1899e58a14a
Lab_06_java <dinhngoctuan2131997@gmail.com>
2021-08-03 19:33:21 +0300

    Initial commit

---
+++ HW3/static/script.js
@@ -0,0 +1,22 @@
+const imgcontainers = Array.from(document.getElementsByClassName('imgcontainer'));
+const modal = document.getElementById('modal-plain');
+const modal_image = document.getElementById('modal-img');
+const closeButton = document.getElementById('close-button');
+imgcontainers.forEach(ele =>{
+    ele.onclick = (e) =>{
+        modal.style.display = 'block';
+        modal_image.src = e.target.src;
+    };
+});
+
+closeButton.onclick = () => {
+    modal.style.display = 'none';
+}
+
+modal.onclick = (e) =>{
+    if(e.target === modal_image){
+        return;
+    }
+    modal.style.display = 'none';
+
+}
